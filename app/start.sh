ac1a091fc0dac041a2249452d6e99824a2458bb4
Vicary Archangel <vicary@yahoo.com>
2017-05-03 00:12:21 +0800

    Adding cups backend for electron-printer

--- app/start.sh
+++ app/start.sh
@@ -5,6 +5,9 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 mkdir -p /data/node-red/user  || true
 mkdir -p /data/node-red/nodes || true
 
+# start cups daemon
+/use/sbin/cupsd
+
 # using local electron module instead of the global electron lets you
 # easily control specific version dependency between your app and electron itself.
 # the syntax below starts an X istance with ONLY our electronJS fired up,
