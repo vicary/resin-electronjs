9fc1c74f20accb41e67ea5477d91272c6a98d03c
Vicary Archangel <vicary@yahoo.com>
2017-05-06 22:21:56 +0800

    Added explicit PPD pinning for Fujifilm ASK-300

--- app/install_printer.py
+++ app/install_printer.py
@@ -17,12 +17,21 @@ else:
 
 printer_name = devices[device_name]['device-info'].replace(' ', '-')
 
-print 'Printer found: ', printer_name
-
 device_id = devices[device_name]['device-id']
-try:
+
+print 'Printer found: ', printer_name, 'device-id: ', device_id
+
+# Hardcode experimental driver model Fujifilm
+if device_id.find('ASK-300') > -1:
+  try:
+    ppds = conn.getPPDs(ppd_product='Fujifilm ASK-300')
+  except cups.IPPError:
+    print 'PPD for ASK-300 not found';
+    exit(1)
+else:
+  try:
     ppds = conn.getPPDs(ppd_device_id=device_id)
-except cups.IPPError:
+  except cups.IPPError:
     print 'No PPD found for this printer'
     exit(1)
 
@@ -34,3 +43,7 @@ conn.acceptJobs(printer_name)
 conn.setDefault(printer_name)
 
 print 'Done.'
+
+# Fujifilm ASK-300 driver in guten print is still experimental
+# The printer itself shows as Fujifilm ASK-Printer, but the driver expects Fujifilm ASK-300
+#lpadmin -E -p "Fujifilm-ASK-300" -v "usb://FUJIFILM/ASK%20Printer" -P "/usr/share/cups/model/gutenprint/5.2/Global/stp-fujifilm-ask-300.5.2.ppd.gz"
